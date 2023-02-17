8479a07508788d80b9e9e0cf82bc1b8464b41bd4
AmirSalari <salari.m.amir@gmail.com>
2023-02-16 19:34:28 -0600

    final

--- Q1.py
+++ Q1.py
@@ -1,8 +1,7 @@
-
 # import requests module
 import requests
   
 # Making a get request
 response = requests.get('https://fortressinfosec.com/')
-# print headers of response
+# print header field of response
 print("X-HS-Cache-Config: ", response.headers['X-HS-Cache-Config'])
