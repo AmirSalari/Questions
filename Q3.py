865675ee3ba2bca2aa7ee3add8d755bac513126b
AmirSalari <salari.m.amir@gmail.com>
2023-02-16 19:37:40 -0600

    changes

--- Q3.py
+++ Q3.py
@@ -7,15 +7,16 @@ tree = html.fromstring(page.content)
 
 # Provide an XPath expression that can be used to locate the first occurrence (and only the first occurrence) of an event entry page.
 part_1 = tree.xpath('//ul[@class="list-recent-events menu"]/li[1]/h3[@class="event-title"]/a[1]/text()')
+print(part_1)
 
 # Provide an XPath expression that can be used to locate the dates of the first event in the page.
 part_2 = tree.xpath('//ul[@class="list-recent-events menu"]/li[1]/p/time/text()')
+print(part_2)
 
 # Provide an XPath expression that can be used to locate all PyCon event occurrences only.
 part_3 = tree.xpath('//ul[@class="list-recent-events menu"]/li/h3[@class="event-title"]/a[starts-with(text(),"PyCon")]/text()')
+print(part_3)
 
 # Provide an Xpath expression that can be used to locate non-online events.
 part_4 = tree.xpath('//ul[@class="list-recent-events menu"]/li/p/span[text() != "Online"]/text()')
-
-
 print(part_4)
--- Q4.py
+++ Q4.py
@@ -1,8 +1,8 @@
 import requests
 import os
 
-URL = "https://f.hubspotusercontent10.net/hubfs/8759415/Major%20U.S.%20Pipline%20Systems%20Shutdown%20After%20Ransomware%20Attack.pdf"
-response = requests.get(URL)
+url = "https://f.hubspotusercontent10.net/hubfs/8759415/Major%20U.S.%20Pipline%20Systems%20Shutdown%20After%20Ransomware%20Attack.pdf"
+response = requests.get(url)
 open("file.pdf", "wb").write(response.content)
 
 file_size = os.path.getsize("./file.pdf")
